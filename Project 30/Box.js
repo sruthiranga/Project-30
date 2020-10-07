672f63837a38147d0084cbb3c8e5db0af64d1b06
sruthiranga <69700420+sruthiranga@users.noreply.github.com>
2020-10-06 22:06:16 -0500

    Add files via upload

---
+++ Project 30/Box.js
@@ -0,0 +1,38 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.Visibility = 255;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      if(this.body<3){
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();    
+      }
+      else{
+          push();
+          World.remove(world, this.body);
+          this.Visibility = this.Visibility-5;
+          tint(255, this.Visibility);
+          pop();
+      }
+    }
+  };
+  
